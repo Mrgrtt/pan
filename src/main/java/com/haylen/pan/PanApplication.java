a6be0a5f2ac1a3041546408fd293a048e66ee1f1
Mrgrtt <grttxiansheng@qq.com>
2019-12-25 19:27:46 +0800

    初始化项目

---
+++ src/main/java/com/haylen/pan/PanApplication.java
@@ -0,0 +1,15 @@
+package com.haylen.pan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author haylen
+ * @date 2019-12-25
+ */
+@SpringBootApplication
+public class PanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PanApplication.class, args);
+    }
+}
---
+++ src/main/java/com/haylen/pan/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.haylen.pan.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * @author haylen
+ * @date 2019-12-25
+ */
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
+        http.headers().cacheControl().disable();
+    }
+}
